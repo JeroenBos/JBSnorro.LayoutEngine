98e56103a715006fee2ece96f72b41ab8db0b72d
Jeroen Bos <jeroen@deepinsight.io>
2021-08-03 20:07:02 +0200

    Assert that css file is resolved

--- SeleniumLayoutEngine.Tests/SeleniumUnderstandingTests.cs
+++ SeleniumLayoutEngine.Tests/SeleniumUnderstandingTests.cs
@@ -21,5 +21,19 @@ namespace SeleniumLayoutEngine.Tests
 
 			Assert.NotNull(element);
 		}
+
+		[Test]
+		public void TestResolveAccessToLocalCSS()
+		{
+			var options = new ChromeOptions();
+			options.AsHeadlessInCI();
+			options.AddArgument("--allow-file-access-from-files");
+			IWebDriver chromeDriver = new ChromeDriver(options);
+			chromeDriver.Navigate().GoToUrl("file:///" + Path.Combine(CurrentPath, "Index.html"));
+
+			// The KaTeX_Main is listed in blatex.css under .katex, which the element with id "test" has
+			IWebElement element = chromeDriver.FindElement(By.Id("test"));
+			Assert.IsTrue(element.GetCssValue("font").Contains("KaTeX_Main"));
+		}
 	}
 }
