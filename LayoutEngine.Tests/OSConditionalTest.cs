f32afbf401db9d606d0e8eebfa7e9ac2641192d0
Jeroen Bos <jer123bos@gmail.com>
2021-08-15 14:15:10 +0200

    Only run publication test on Windows

---
+++ LayoutEngine.Tests/OSConditionalTest.cs
@@ -0,0 +1,62 @@
+using System.IO;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using JBSnorro;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+class TestOnWindowsOnly
+#if WINDOWS
+	: TestAttribute
+#else
+	: Attribute
+#endif
+{
+
+}
+class TestOnLinuxOnly
+#if LINUX
+	: Attribute
+#else
+	: TestAttribute
+#endif
+{
+
+}
+
+class DefineConstantsTests
+{
+#if WINDOWS
+	[Test]
+	public void TestOSWindows()
+	{
+		Assert.IsTrue(OperatingSystem.IsWindows());
+		Assert.IsFalse(OperatingSystem.IsLinux());
+	}
+#else
+	[Test]
+	public void TestOSWindows()
+	{
+		Assert.IsFalse(OperatingSystem.IsWindows());
+		Assert.IsTrue(OperatingSystem.IsLinux());
+	}
+#endif
+
+#if LINUX
+	[Test]
+	public void TestOSLinux()
+	{
+		Assert.IsFalse(OperatingSystem.IsWindows());
+		Assert.IsTrue(OperatingSystem.IsLinux());
+	}
+#else
+	[Test]
+	public void TestOSLinux()
+	{
+		Assert.IsTrue(OperatingSystem.IsWindows());
+		Assert.IsFalse(OperatingSystem.IsLinux());
+	}
+#endif
+}
