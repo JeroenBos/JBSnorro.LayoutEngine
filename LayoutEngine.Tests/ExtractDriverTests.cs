14ad229f5ff2b2739c68147b543259fd51eb3710
Jeroen Bos <jer123bos@gmail.com>
2021-10-20 20:34:00 +0200

    New hash

--- LayoutEngine.Tests/ExtractDriverTests.cs
+++ LayoutEngine.Tests/ExtractDriverTests.cs
@@ -22,7 +22,7 @@ public class ExtractDriverTests
 
 		unchecked
 		{
-			nuint expectedHashCode = OperatingSystem.IsWindows() ? (nuint)13943427872559576225 : (nuint)16353916093246116963;
+			nuint expectedHashCode = OperatingSystem.IsWindows() ? (nuint)13943427872559576225 : (nuint)1290074335871784207;
 			// the following does not work in CI, because the path has 1 extra depth (the runtime identifier):
 			// int expectedHashCode = $"../../../../LayoutEngine/chromedriver{extension}".ComputeFileHashCode();
 
