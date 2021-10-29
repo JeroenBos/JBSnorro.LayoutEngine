79246b423f0c9612c794a7207a364bd9ba308e50
Jeroen Bos <jer123bos@gmail.com>
2021-10-29 15:06:43 +0200

    Demonstrate (non)headless chromedriver bug in getBoundingClient()

--- LayoutEngine.Tests/MeasurerTests.cs
+++ LayoutEngine.Tests/MeasurerTests.cs
@@ -9,56 +9,96 @@ using LayoutEngine = JBSnorro.Web.LayoutEngine;
 
 public class MeasurerTests
 {
-	class DummyMeasurer : IMeasurer<object>
-	{
-		object IMeasurer<object>.Measure(IWebElement element, RemoteWebDriver driver) => element;
-	}
-	[Test]
-	public void BodylessHtmlDoesntRaiseException()
-	{
-		// Arrange
-		using var driver = LayoutEngine.OpenPage(Path.GetFullPath("Bodyless.html"));
-		IMeasurer<object> measurer = new DummyMeasurer();
+    class DummyMeasurer : IMeasurer<object>
+    {
+        object IMeasurer<object>.Measure(IWebElement element, RemoteWebDriver driver) => element;
+    }
+    [Test]
+    public void BodylessHtmlDoesntRaiseException()
+    {
+        // Arrange
+        using var driver = LayoutEngine.OpenPage(Path.GetFullPath("Bodyless.html"));
+        IMeasurer<object> measurer = new DummyMeasurer();
 
-		// Act
-		IWebElement body = (IWebElement)measurer.Measure(driver);
+        // Act
+        IWebElement body = (IWebElement)measurer.Measure(driver);
 
-		// Assert
-		Assert.IsNotNull(body);
-		Assert.AreEqual("body", body!.TagName);
-	}
+        // Assert
+        Assert.IsNotNull(body);
+        Assert.AreEqual("body", body!.TagName);
+    }
 }
 
 public class BoundingRectsMeasurerTests
 {
-	[Test]
-	public void Explicit_Sizes_Can_Be_Read_From_Div()
-	{
-		// Arrange
-		using var driver = LayoutEngine.OpenPage(Path.GetFullPath("OneElementWithSizes.html"));
-		var measurer = new BoundingRectMeasurer();
+    [Test]
+    public void Explicit_Sizes_Can_Be_Read_From_Div()
+    {
+        // Arrange
+        using var driver = LayoutEngine.OpenPage(Path.GetFullPath("OneElementWithSizes.html"));
+        var measurer = new BoundingRectMeasurer();
 
-		// Act
-		var sizesByXPath = measurer.Measure(driver)!;
+        // Act
+        var sizesByXPath = measurer.Measure(driver)!;
 
-		// Assert
-		const string divXPath = "/HTML[1]/BODY[1]/DIV[1]";
-		Assert.IsTrue(sizesByXPath.ContainsKey(divXPath));
+        // Assert
+        const string divXPath = "/HTML[1]/BODY[1]/DIV[1]";
+        Assert.IsTrue(sizesByXPath.ContainsKey(divXPath));
 
-		TaggedRectangle divSizes = sizesByXPath[divXPath];
-		Assert.AreEqual(divSizes, new TaggedRectangle("div", 8, 8, 400.296875f, 300.5f));
-	}
+        TaggedRectangle divSizes = sizesByXPath[divXPath];
+        Assert.AreEqual(divSizes, new TaggedRectangle("div", 8, 8, 400.296875f, 300.5f));
+    }
 }
 
 public class TaggedRectangleFormattingTests
 {
-	[Test]
-	public void TestTaggedRectangleormatter()
-	{
-		var rect = new TaggedRectangle("div", 1, 0, 1 / 3f, 0.2f);
+    [Test]
+    public void TestTaggedRectangleormatter()
+    {
+        var rect = new TaggedRectangle("div", 1, 0, 1 / 3f, 0.2f);
 
-		var formatted = rect.Format();
+        var formatted = rect.Format();
 
-		Assert.AreEqual("div,1,0,0.33333334,0.2", formatted);
-	}
+        Assert.AreEqual("div,1,0,0.33333334,0.2", formatted);
+    }
+}
+
+
+public class DemonstrateChromedriverBug
+{
+    [Test]
+    [TestCase(false)]
+#if CI
+    [TestCase(true)]
+#endif
+    public void Headless_And_Headfull_Chromedrivers_Round_Element_Height_Differently(bool showHead)
+    {
+        // Arrange
+        using var driver = LayoutEngine.OpenPage(Path.GetFullPath("OneElementWithFractionalHeight.html"), showHead);
+        var measurer = new BoundingRectMeasurer();
+
+        // Act
+        var sizesByXPath = measurer.Measure(driver)!;
+
+        // Assert
+        const string spanWithClassKatex_HTMLXPath = "/HTML[1]/BODY[1]/SPAN[1]"; // class=katex-html
+        Assert.IsTrue(sizesByXPath.ContainsKey(spanWithClassKatex_HTMLXPath));
+
+        TaggedRectangle divSizes = sizesByXPath[spanWithClassKatex_HTMLXPath];
+
+        Assert.AreEqual(divSizes.Tagname, "SPAN");
+        Assert.AreEqual(divSizes.X, 0);
+        Assert.AreEqual(divSizes.Y, 0);
+        
+        if (showHead)
+        {
+            Assert.AreEqual(divSizes.Width, 25.71875f);
+            Assert.AreEqual(divSizes.Height, 17.333334f);
+        }
+        else
+        {
+            Assert.AreEqual(divSizes.Width, 25.703125f);
+            Assert.AreEqual(divSizes.Height, 17f);
+        }
+    }
 }
--- LayoutEngine/LayoutEngine.cs
+++ LayoutEngine/LayoutEngine.cs
@@ -8,140 +8,143 @@ using static JBSnorro.Extensions;
 
 namespace JBSnorro.Web
 {
-	public static class LayoutEngine
-	{
-		/// <summary>
-		/// Opens the index.html page in the specified directory for consumption by a <see cref="IMeasurer{T}"/>.
-		/// </summary>
-		public static RemoteWebDriver OpenDir(string dir)
-		{
-			if (dir == null)
-				throw new ArgumentNullException(nameof(dir));
-			if (!Directory.Exists(dir))
-				if (File.Exists(dir))
-					throw new ArgumentException($"The path is a file, not a directory: '{dir}'", "--dir");
-				else
-					throw new ArgumentException($"The directory does not exist: '{dir}'", "--dir");
+    public static class LayoutEngine
+    {
+        /// <summary>
+        /// Opens the index.html page in the specified directory for consumption by a <see cref="IMeasurer{T}"/>.
+        /// </summary>
+        public static RemoteWebDriver OpenDir(string dir)
+        {
+            if (dir == null)
+                throw new ArgumentNullException(nameof(dir));
+            if (!Directory.Exists(dir))
+                if (File.Exists(dir))
+                    throw new ArgumentException($"The path is a file, not a directory: '{dir}'", "--dir");
+                else
+                    throw new ArgumentException($"The directory does not exist: '{dir}'", "--dir");
 
 
-			string filePath1 = Path.GetFullPath(Path.Combine(dir, "index.html"));
-			string filePath2 = Path.GetFullPath(Path.Combine(dir, "Index.html"));
-			bool file1Exists = File.Exists(filePath1);
-			bool file2Exists = !file1Exists && File.Exists(filePath2);
-			if (!file1Exists && !file2Exists)
-			{
-				throw new ArgumentException($"No 'index.html' or 'Index.html' file found in dir '{dir}'");
-			}
-			return OpenPage(file1Exists ? filePath1 : filePath2);
-		}
-		/// <summary>
-		/// Opens the website at the specified path for consumption by a <see cref="IMeasurer{T}"/>.
-		/// </summary>
-		public static RemoteWebDriver OpenPage(string fullPath)
-		{
-			if (fullPath == null)
-				throw new ArgumentNullException(nameof(fullPath));
-			if (fullPath.StartsWith("file://"))
-				throw new ArgumentException($"{nameof(fullPath)} shouldn't start with 'file://'", nameof(fullPath));
-			if (!IsFullPath(fullPath))
-				throw new ArgumentException($"'{fullPath}' is not a full path", nameof(fullPath));
-			if (!File.Exists(fullPath))
-				throw new ArgumentException($"The file does not exist: '{fullPath}'", nameof(fullPath));
+            string filePath1 = Path.GetFullPath(Path.Combine(dir, "index.html"));
+            string filePath2 = Path.GetFullPath(Path.Combine(dir, "Index.html"));
+            bool file1Exists = File.Exists(filePath1);
+            bool file2Exists = !file1Exists && File.Exists(filePath2);
+            if (!file1Exists && !file2Exists)
+            {
+                throw new ArgumentException($"No 'index.html' or 'Index.html' file found in dir '{dir}'");
+            }
+            return OpenPage(file1Exists ? filePath1 : filePath2);
+        }
+        /// <summary>
+        /// Opens the website at the specified path for consumption by a <see cref="IMeasurer{T}"/>.
+        /// </summary>
+        public static RemoteWebDriver OpenPage(string fullPath, bool showHead = false)
+        {
+            if (fullPath == null)
+                throw new ArgumentNullException(nameof(fullPath));
+            if (fullPath.StartsWith("file://"))
+                throw new ArgumentException($"{nameof(fullPath)} shouldn't start with 'file://'", nameof(fullPath));
+            if (!IsFullPath(fullPath))
+                throw new ArgumentException($"'{fullPath}' is not a full path", nameof(fullPath));
+            if (!File.Exists(fullPath))
+                throw new ArgumentException($"The file does not exist: '{fullPath}'", nameof(fullPath));
 
-			var driver = CreateDriver(); // don't dispose; it's returned
-			System.Diagnostics.Trace.WriteLine($"Opening file '{fullPath.ToFileSystemPath()}'");
-			driver.Navigate().GoToUrl(fullPath.ToFileSystemPath());
-			return driver;
-		}
-		private static ChromeDriver CreateDriver()
-		{
-			// create service before creating ChromeOptions due to its static ctor crashing otherwise. Don't dispose; is returned
-			var service = CreateDriverService();
-			var options = new ChromeOptions();
-			options.AddArgument("--headless");
-			options.AddArgument("--disable-gpu");
-			options.AddArgument("--allow-file-access-from-files");
+            var driver = CreateDriver(headless: !showHead); // don't dispose; it's returned
+            System.Diagnostics.Trace.WriteLine($"Opening file '{fullPath.ToFileSystemPath()}'");
+            driver.Navigate().GoToUrl(fullPath.ToFileSystemPath());
+            return driver;
+        }
+        private static ChromeDriver CreateDriver(bool headless)
+        {
+            // create service before creating ChromeOptions due to its static ctor crashing otherwise. Don't dispose; is returned
+            var service = CreateDriverService();
+            var options = new ChromeOptions();
+            if (headless)
+            {
+                options.AddArgument("--headless");
+            }
+            options.AddArgument("--disable-gpu");
+            options.AddArgument("--allow-file-access-from-files");
 
-			var driver = new ChromeDriver(service, options);
-			driver.AssertBrowserAndDriverVersionsCompatible();
-			return driver;
-		}
-		private static ChromeDriverService CreateDriverService()
-		{
-			// The default services searches in the directory of the executing binary, and PATH.
-			// But, the published artifact is extracted somewhere and run there, which is where Selenium searches
-			// Here we specify to find it next to the artifact instead
-			return ChromeDriverService.CreateDefaultService(Directory.GetCurrentDirectory());
-		}
-		/// <summary>
-		/// Gets all the bounding client rectangles of the html elements in the specified driver by element xpath.
-		/// </summary>
-		public static IReadOnlyDictionary<string, TaggedRectangle> MeasureBoundingClientsRects(RemoteWebDriver driver)
-		{
-			return new BoundingRectMeasurer().Measure(driver);
-		}
-		/// <summary>
-		/// Gets all the bounding client rectangles of the html elements in the specified driver order by element xpath.
-		/// </summary>
-		public static IEnumerable<TaggedRectangle> GetSortedMeasuredBoundingClientsRects(RemoteWebDriver driver)
-		{
-			return MeasureBoundingClientsRects(driver)
-					  .OrderBy(pair => pair.Key)
-					  .Select(pair => pair.Value);
-		}
+            var driver = new ChromeDriver(service, options);
+            driver.AssertBrowserAndDriverVersionsCompatible();
+            return driver;
+        }
+        private static ChromeDriverService CreateDriverService()
+        {
+            // The default services searches in the directory of the executing binary, and PATH.
+            // But, the published artifact is extracted somewhere and run there, which is where Selenium searches
+            // Here we specify to find it next to the artifact instead
+            return ChromeDriverService.CreateDefaultService(Directory.GetCurrentDirectory());
+        }
+        /// <summary>
+        /// Gets all the bounding client rectangles of the html elements in the specified driver by element xpath.
+        /// </summary>
+        public static IReadOnlyDictionary<string, TaggedRectangle> MeasureBoundingClientsRects(RemoteWebDriver driver)
+        {
+            return new BoundingRectMeasurer().Measure(driver);
+        }
+        /// <summary>
+        /// Gets all the bounding client rectangles of the html elements in the specified driver order by element xpath.
+        /// </summary>
+        public static IEnumerable<TaggedRectangle> GetSortedMeasuredBoundingClientsRects(RemoteWebDriver driver)
+        {
+            return MeasureBoundingClientsRects(driver)
+                      .OrderBy(pair => pair.Key)
+                      .Select(pair => pair.Value);
+        }
 
-		public static void AssertBrowserAndDriverVersionsCompatible(this ChromeDriver driver)
-		{
-			string warningReason;
+        public static void AssertBrowserAndDriverVersionsCompatible(this ChromeDriver driver)
+        {
+            string warningReason;
 
 
-			var browserVersion = driver.Capabilities.GetCapability("browserVersion") as string;
-			if (browserVersion != null)
-			{
-				var browser = driver.Capabilities.GetCapability("chrome") as Dictionary<string, object>;
-				if (browser != null)
-				{
-					if (browser.TryGetValue("chromedriverVersion", out object? driverVersionObj))
-					{
-						if (driverVersionObj is string { Length: >= 3 } driverVersion)
-						{
-							if (browserVersion == driverVersion[..browserVersion.Length])
-							{
-								return; // OK. versions are identical
-							}
-							if (browserVersion[..2] == driverVersion[..2])
-							{
-								Console.WriteLine($"Warning: browser minor version and driver minor version differ: {browserVersion} vs {driverVersion[..browserVersion.Length]}");
-								return; // OK, major versions are identical
-							}
-							else
-							{
-								string error = $"The browser and driver versions aren't compatible: {browserVersion} vs {driverVersion}";
-								Console.WriteLine(error);
-								throw new InvalidOperationException(error);
-							}
-						}
-						else
-						{
-							warningReason = "capability 'chrome.chromedriverVersion' is not a string";
-						}
-					}
-					else
-					{
-						warningReason = "capability 'chrome.chromedriverVersion' not found";
-					}
-				}
-				else
-				{
-					warningReason = "capability 'chrome' not found.";
-				}
-			}
-			else
-			{
-				warningReason = "capability 'browserVersion' not found.";
-			}
+            var browserVersion = driver.Capabilities.GetCapability("browserVersion") as string;
+            if (browserVersion != null)
+            {
+                var browser = driver.Capabilities.GetCapability("chrome") as Dictionary<string, object>;
+                if (browser != null)
+                {
+                    if (browser.TryGetValue("chromedriverVersion", out object? driverVersionObj))
+                    {
+                        if (driverVersionObj is string { Length: >= 3 } driverVersion)
+                        {
+                            if (browserVersion == driverVersion[..browserVersion.Length])
+                            {
+                                return; // OK. versions are identical
+                            }
+                            if (browserVersion[..2] == driverVersion[..2])
+                            {
+                                Console.WriteLine($"Warning: browser minor version and driver minor version differ: {browserVersion} vs {driverVersion[..browserVersion.Length]}");
+                                return; // OK, major versions are identical
+                            }
+                            else
+                            {
+                                string error = $"The browser and driver versions aren't compatible: {browserVersion} vs {driverVersion}";
+                                Console.WriteLine(error);
+                                throw new InvalidOperationException(error);
+                            }
+                        }
+                        else
+                        {
+                            warningReason = "capability 'chrome.chromedriverVersion' is not a string";
+                        }
+                    }
+                    else
+                    {
+                        warningReason = "capability 'chrome.chromedriverVersion' not found";
+                    }
+                }
+                else
+                {
+                    warningReason = "capability 'chrome' not found.";
+                }
+            }
+            else
+            {
+                warningReason = "capability 'browserVersion' not found.";
+            }
 
-			Console.WriteLine("Warning: browser version and driver version compatibility could not be determined: " + warningReason);
-		}
-	}
+            Console.WriteLine("Warning: browser version and driver version compatibility could not be determined: " + warningReason);
+        }
+    }
 }
